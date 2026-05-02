82f7bbdb66f14868aa66511a14d6af06652c9805
delavegavolochenlucas <delavegavolochenlucas@gmail.com>
2026-05-02 17:39:11 -0300

    Neo: Just A Very Intelligent System

--- listas.py
+++
@@ -1,7 +0,0 @@
-# Listas são usadas para armazenar múltiplos valores em uma única variável, e são definidas com colchetes [].
-
-frutas = ["maça", "banana", "laranja", "uva", "morango"]
-for fruta in frutas:
-    print(fruta)
-
-# nesse código, criamos uma lista chamada frutas, que contém cinco valores (maça, banana, laranja, uva e morango), e utilizamos um loop for para percorrer cada elemento da lista, e o print serve para mostrar cada fruta no terminal. O for pega cada valor da lista frutas, e coloca na variável escolhida (fruta nesse caso), e a cada loop ele é atualizado com o próximo valor da lista.
